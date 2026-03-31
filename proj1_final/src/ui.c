bab2c114aaa7126fc2479ff4861bf353c2ff4302
Luiz Pulga <162224053+luiz-pulga@users.noreply.github.com>
2026-03-31 11:41:00 -0300

    Update ui.c

--- proj1_final/src/ui.c
+++ proj1_final/src/ui.c
@@ -1,1 +1,12 @@
+#include "ui.h"
 
+#include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <stdio.h>
+#include <string.h>
+
+#define HIST_MARGIN_X 25
+#define HIST_MARGIN_TOP 30
+#define HIST_PLOT_WIDTH 400
+#define HIST_PLOT_HEIGHT 220
+#define TEXT_WRAP_WIDTH 400
