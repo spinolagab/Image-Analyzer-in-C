d7dbffcf8d18b09736afdd95c6262c35f0626446
Luiz Pulga <162224053+luiz-pulga@users.noreply.github.com>
2026-03-31 11:37:03 -0300

    Create ui.h

---
+++ proj1_final/src/ui.h
@@ -0,0 +1,13 @@
+#ifndef UI_H
+#define UI_H
+
+#include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <stdbool.h>
+
+#include "image_processing.h"
+
+#define HIST_WINDOW_WIDTH 460
+#define HIST_WINDOW_HEIGHT 620
+#define BUTTON_W 180
+#define BUTTON_H 48
