a1fb2214667c8d38c5f4198bfb3072ae60d02252
Gabriel Spinola <134285947+spinolagab@users.noreply.github.com>
2026-04-01 01:18:28 +0000

    Initial commit on APPFiles

---
+++ proj1_final/src/app.h
@@ -0,0 +1,6 @@
+#ifndef APP_H
+#define APP_H
+
+int run_app(int argc, char *argv[]);
+
+#endif
---
+++ proj1_final/src/font_utils.h
@@ -0,0 +1,6 @@
+#ifndef FONT_UTILS_H
+#define FONT_UTILS_H
+
+const char *find_font_path(void);
+
+#endif
